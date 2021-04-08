a4061b05ee8cb0b6b02a79c5e9b19c04bb9be554
Kurbee8867 <68202450+yes-bit@users.noreply.github.com>
2021-04-08 11:45:07 -0600

    Update
    Added Base Files

---
+++ selfbot.py
@@ -0,0 +1,25 @@
+import discord, json, pyfiglet
+
+from discord.ext import commands
+
+bot = commands.Bot(description="we do a little trolling", command_prefix="./", self_bot=True)
+bot.remove_command('help')
+
+@bot.event
+async def on_ready():
+    print(f'Done')
+
+@bot.event
+async def on_message(message):
+    if '!drop' in message.content.lower():
+        await message.channel.send('!pickup')
+
+
+
+
+
+with open('./config.json') as f:
+    config = json.load(f)
+
+token = config.get('token')
+bot.run(token, bot=False)
